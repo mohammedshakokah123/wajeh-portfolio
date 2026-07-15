f15dc2a692bbb80948a040c52073bbedd4a2a60b
mohammed.sh <mohammedshakokah@gmail.com>
2026-07-16 02:43:32 +0300

    edit

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -7,6 +7,7 @@ import Contact from "@/components/Contact";
 import Footer from "@/components/Footer";
 
 export default function Home() {
+  console.log("hello")
   return (
     <>
       <Header />
